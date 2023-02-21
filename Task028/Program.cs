6558e59bfc8b2af3de9511d5fb5f6fa152ad4750
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-21 13:08:30 +0500

    Task028 Product of numbers from 1(-1) to N

---
+++ Task028/Program.cs
@@ -0,0 +1,19 @@
+﻿int multiplication(int N)
+{
+    int sum = 1;
+    int i = 1;
+    if (N < 0)
+    {
+        i = N;
+        N = -1;
+    }
+    for (; i <= N; i++)
+    {   
+        sum = sum * i;
+    }
+    return sum;
+}
+
+Console.Write("Произведение чисел от 1(-1) до N, ведите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write($"{multiplication(N)}");
