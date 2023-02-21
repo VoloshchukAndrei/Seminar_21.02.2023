f59bb8864f75ee2561f6f66960862c51dcab8d99
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-21 18:26:11 +0500

    Task030 ZeroOneArray

---
+++ Task030/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int N = number;
+
+int[] array = ZeroOneArray(N);
+
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+int[] ZeroOneArray (int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(2);
+    }
+    return result;
+}
+
+
