a242c469988d1a4bbccc94bcdc75b68371888cee
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-21 12:43:34 +0500

    Task026 Quantity

---
+++ Task026/Program.cs
@@ -0,0 +1,18 @@
+﻿int Quantity(int N)
+{
+    int count = 0;
+    if (N < 1)
+    {
+        N *= (-1)
+    }
+    while (N >= 1)
+    {
+        N = N / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Определение количества символов в числе, ведите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write($"{Quantity(N)}");
